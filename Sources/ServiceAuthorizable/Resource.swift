086ad272b01bac586f8064004d3701ca78eec2cb
Sasha Riabchuk <oriabchuk@csas.cz>
2023-10-16 13:09:54 +0200

    Remove cases

--- Sources/ServiceAuthorizable/Resource.swift
+++ Sources/ServiceAuthorizable/Resource.swift
@@ -6,17 +6,5 @@
 //
 
 public enum Resource: String, Equatable {
-    case cshub = "urn:csas:apps:cshub"
-    case tko = "urn:csas:apps:tk_tablet"
-    case biometrySign = "urn:csas:apps:biosign"
-    case pairing = "urn:csas:apps:pairing"
-    case mzf = "urn:csas:apps:mzf"
-    case mzu = "urn:csas:apps:mzu"
-    case afp = "urn:csas:apps:afp"
-    case lcons = "urn:csas:apps:store:branch"
-    case bidentity = "urn:csas:apps:reg"
-    case flexi = "urn:csas:apps:flexi_tablet"
-    case nzp = "urn:csas:apps:hades"
-    case bidentabove = "urn:csas:apps:idm"
-    case staveb = "Store_cz"
+    case api
 }
